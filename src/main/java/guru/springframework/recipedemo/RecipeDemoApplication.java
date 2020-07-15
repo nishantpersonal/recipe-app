197e2eeb2fcf58ba9f5629856390b18668a0fb7c
nishantpersonal <nishantvenugopalnair@gmail.com>
2020-07-15 20:46:32 +0530

    Some other changes

--- src/main/java/guru/springframework/recipedemo/RecipeDemoApplication.java
+++ src/main/java/guru/springframework/recipedemo/RecipeDemoApplication.java
@@ -1,11 +1,12 @@
 package guru.springframework.recipedemo;
 
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecipeDemoApplication {
-
+public class RecipeDemoApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeDemoApplication.class, args);
 
--- src/main/java/guru/springframework/recipedemo/domain/Ingredient.java
+++ src/main/java/guru/springframework/recipedemo/domain/Ingredient.java
@@ -1,9 +1,13 @@
 package guru.springframework.recipedemo.domain;
 
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Getter
+@Setter
 @Entity
 public class Ingredient {
 
@@ -32,44 +36,4 @@ public class Ingredient {
     }
 
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public UnitOfMeasure getUom() {
-        return uom;
-    }
-
-    public void setUom(UnitOfMeasure unitOfMeasure) {
-        this.uom = unitOfMeasure;
-    }
 }
